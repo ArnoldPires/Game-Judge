dbe47fc105439fb9614a6d1e0d6f33ea2e0d5336
Arnold <arnold.pires92@gmail.com>
2019-06-15 17:09:35 -0400

    pushing latest changes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const gameJudge = [
 
 ]
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,7 +10,14 @@ app.use('/css', express.static('css'));
 app.use('/js', express.static('js'));
 app.use(express.static('public'));
 
-app.use(express.static('views'))
+//mongoose setup
+const mongoose = require('mongoose');
+const url = 'mongodb+srv://Arnold:action1234@game-judge-3yhpt.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(url, {useNewUrlParser: true})
+.then(() => console.log('MongoDB Connected'))
+.catch((err) => console.log(err));
+
+app.use(express.static('views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
