3bc061e4f9e8e78f17dcc22a4cac861045fe5b50
Arnold <arnold.pires92@gmail.com>
2019-06-18 18:55:03 -0400

    changed where the index file is

---
+++ public/js/comment.js
@@ -0,0 +1,11 @@
+// require Mongoose
+var mongoose = require('mongoose');
+
+// set up database structure for comments
+var commentSchema = mongoose.Schema({
+  text: String,
+  author: String
+});
+
+// Export the comment model to app.js
+module.exports = mongoose.routes('Comment', commentSchema);
