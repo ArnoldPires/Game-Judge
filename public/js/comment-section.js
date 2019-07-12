5cf532b26aac90ab66469a4091d18546d1ba1a6c
ArnoldPires <arnold.pires92@gmail.com>
2019-07-12 14:55:19 -0400

    moved timestamp

--- public/js/comment-section.js
+++ public/js/comment-section.js
@@ -4,16 +4,16 @@ function post() {
   let comment = document.getElementById('comment').value;
   let commentTimeStamp = document.getElementById('commentTimeStamp').value;
   //Create new tags
-  let d = new Date();
-  let p = document.createElement("p");
   let h1 = document.createElement("strong");
   let h3 = document.createElement("p");
+  let p = document.createElement("p");
+  let d = new Date();
   //put user into in the tags
-  p.innerHTML = d.toString();
   h1.innerHTML = username;
 	h3.innerHTML = comment;
+  p.innerHTML = d.toString();
   //put the tags into a new div for people to see
-  document.getElementById("commentTimeStamp").appendChild(p);
   document.getElementById("commentTimeStamp").appendChild(h1);
 	document.getElementById("commentTimeStamp").appendChild(h3);
+  document.getElementById("commentTimeStamp").appendChild(p);
 };
